4bef06a6e06c9df2b86abb657915baacf8ead516
ketan <ketanpardhi7@gmail.com>
2023-11-21 21:23:27 +0530

    search and traversal function

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -29,6 +29,48 @@ struct Node *insert(struct Node *root, int key)
     return root;
 }
 
+struct Node *search(struct Node *root, int key)
+{
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+void inOrderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+void preOrderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+
+void postOrderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+
 int main()
 {
     struct Node *root = NULL;
@@ -39,6 +81,25 @@ int main()
     insert(root, 20);
     insert(root, 40);
     insert(root, 70);
+    // Search
+    int keyToSearch = 40;
+    struct Node *searchResult = search(root, keyToSearch);
+    if (searchResult)
+        printf("%d found in the BST.\n", keyToSearch);
+    else
+        printf("%d not found in the BST.\n", keyToSearch);
 
+    // Traversals
+    printf("In-order traversal: ");
+    inOrderTraversal(root);
+    printf("\n");
+
+    printf("Pre-order traversal: ");
+    preOrderTraversal(root);
+    printf("\n");
+
+    printf("Post-order traversal: ");
+    postOrderTraversal(root);
+    printf("\n");
     return 0;
 }
