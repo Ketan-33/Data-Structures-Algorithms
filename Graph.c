9514d9b218f434a35b7a619a0ea9bdb7cc0c6054
ketan <ketanpardhi7@gmail.com>
2023-11-29 21:01:24 +0530

    BSF function

--- Graph.c
+++ Graph.c
@@ -42,3 +42,35 @@ void DFS(struct Graph *graph, int visited[MAX_VERTICES], int currentVertex)
         }
     }
 }
+
+void BFS(struct Graph *graph, int startVertex)
+{
+    int visited[MAX_VERTICES] = {0};
+    int queue[MAX_VERTICES];
+    int front = -1, rear = -1;
+
+    printf("BFS Traversal starting from vertex %d: ", startVertex);
+
+    // Enqueue the start vertex
+    queue[++rear] = startVertex;
+    visited[startVertex] = 1;
+
+    while (front != rear)
+    {
+        // Dequeue a vertex and print it
+        int currentVertex = queue[++front];
+        printf("%d ", currentVertex);
+
+        // Enqueue adjacent vertices
+        for (int i = 0; i < graph->vertices; i++)
+        {
+            if (graph->adjacencyMatrix[currentVertex][i] == 1 && !visited[i])
+            {
+                queue[++rear] = i;
+                visited[i] = 1;
+            }
+        }
+    }
+
+    printf("\n");
+}
